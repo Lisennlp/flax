8b830a15240513e605093e22c00fef3d40c93fc4
lishengping2010@163.com <lishengping2010@163.com>
2024-01-13 16:54:04 +0800

    print config

--- examples/wmt/train.py
+++ examples/wmt/train.py
@@ -524,6 +524,7 @@ def train_and_evaluate(config: ml_collections.ConfigDict, workdir: str):
       de_dynamic_compose1=config.de_dynamic_compose1,
       de_dynamic_compose2=config.de_dynamic_compose2,
   )
+  print(f'train_config: {train_config}')
   eval_config = train_config.replace(deterministic=True)
   predict_config = train_config.replace(deterministic=True, decode=True)
 
